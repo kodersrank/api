c92a43a84b8b22d07c499e2fa6338799b25bb587
Dan Nagaoka <dan.nagaoka.0@gmail.com>
2024-08-08 21:55:57 -0300

    fix: use method chaining, listen to port

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,10 @@ import { AddSecretSchema } from './schemas/add-secret-schema';
 import { GetSecretSchema } from './schemas/get-secret-schema';
 
 // Initialize Elysia server
-const app = new Elysia();
-
-// Define routes with mock responses
-app.post('/secret', addSecretHandler, AddSecretSchema);
-
-app.get('/secret/:hash', getSecretHandler, GetSecretSchema);
+const app = new Elysia()
+  .post('/secret', addSecretHandler, AddSecretSchema)
+  .get('/secret/:hash', getSecretHandler, GetSecretSchema)
+  .listen(3333);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
