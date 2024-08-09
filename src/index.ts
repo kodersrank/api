9324e03a7055fa8a066e750eecfa160ce86a05de
Dan Nagaoka <dan.nagaoka.0@gmail.com>
2024-08-09 02:22:17 -0300

    feat: add XML response plugin

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,11 @@ import { addSecretHandler } from './handlers/add-secret-handler';
 import { getSecretHandler } from './handlers/get-secret-handler';
 import { AddSecretSchema } from './schemas/add-secret-schema';
 import { GetSecretSchema } from './schemas/get-secret-schema';
+import { xml } from 'elysia-xml';
 
 // Initialize Elysia server
 const app = new Elysia()
+  .use(xml())
   .post('/secret', addSecretHandler, AddSecretSchema)
   .get('/secret/:hash', getSecretHandler, GetSecretSchema)
   .listen(3333);
