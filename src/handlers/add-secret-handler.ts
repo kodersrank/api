35ee2a07279235cb9dfa312290255a3e3793f85d
Dan Nagaoka <dan.nagaoka.0@gmail.com>
2024-08-09 02:02:38 -0300

    feat(addSecretHandler): implement handler with JSON response

--- src/handlers/add-secret-handler.ts
+++ src/handlers/add-secret-handler.ts
@@ -1,6 +1,32 @@
 import type { Handler } from 'elysia';
+import { db } from '../db';
+import type { Secret } from '../types/secret.interface';
 
-export const addSecretHandler: Handler = async () => {
-  // Mock response
-  return { message: 'Secret added successfully' };
+export const addSecretHandler: Handler = async ({ body }) => {
+  const { secret, expireAfterViews, expireAfter } = body as Secret;
+
+  // Insert secret into the database
+  const [id] = await db('secrets')
+    .insert({
+      secret,
+      expireAfterViews,
+      expireAfter,
+      createdAt: new Date(),
+      expiresAt:
+        expireAfter > 0 ? new Date(Date.now() + expireAfter * 60000) : null,
+    })
+    .returning('id');
+
+  const response = {
+    hash: id,
+    secretText: secret,
+    createdAt: new Date().toISOString(),
+    expiresAt:
+      expireAfter > 0
+        ? new Date(Date.now() + expireAfter * 60000).toISOString()
+        : null,
+    remainingViews: expireAfterViews,
+  };
+
+  return response;
 };
---
+++ src/types/secret.interface.ts
@@ -0,0 +1,5 @@
+export interface Secret {
+  secret: string;
+  expireAfterViews: number;
+  expireAfter: number;
+}
