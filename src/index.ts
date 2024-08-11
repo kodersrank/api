2550696dac1d8b7709f5ca60f25de5483bf43b3b
Dan Nagaoka <dan.nagaoka.0@gmail.com>
2024-08-11 04:37:57 -0300

    feat: use swagger plugin + group routes to avoid XML parsing on swagger page

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,17 @@ import { getSecretHandler } from './handlers/get-secret-handler';
 import { AddSecretSchema } from './schemas/add-secret-schema';
 import { GetSecretSchema } from './schemas/get-secret-schema';
 import { xml } from 'elysia-xml';
+import swagger from '@elysiajs/swagger';
 
 // Initialize Elysia server
 const app = new Elysia()
-  .use(xml())
-  .post('/secret', addSecretHandler, AddSecretSchema)
-  .get('/secret/:hash', getSecretHandler, GetSecretSchema)
+  .use(swagger())
+  .group('/secret', (app) =>
+    app
+      .use(xml())
+      .post('/', addSecretHandler, AddSecretSchema)
+      .get('/:hash', getSecretHandler, GetSecretSchema)
+  )
   .listen(3333);
 
 console.log(
