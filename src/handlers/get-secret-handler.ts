74e2d93e5d8624e866ad7bd66630a007df71d357
Dan Nagaoka <dan.nagaoka.0@gmail.com>
2024-08-14 02:16:03 -0300

    test(getSecretHandler): add basic test scenarios

--- src/handlers/get-secret-handler.ts
+++ src/handlers/get-secret-handler.ts
@@ -22,7 +22,7 @@ export const getSecretHandler: Handler = async ({ error, params }) => {
   } = secret;
 
   // check expiration based on TTL
-  if (expiresAt && new Date(expiresAt) <= new Date()) {
+  if (expiresAt && new Date(expiresAt) <= new Date(Date.now())) {
     return error(410, 'secret has expired');
   }
 
